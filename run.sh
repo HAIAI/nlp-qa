1a8f3bffa2ea3aa8b6abdc08e606f1412de156f6
HenryX <Hai.Xiao@amd.com>
2021-04-26 11:44:56 -0400

    Add run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+python main.py \
+    --use_gpu \
+    --model "baseline" \
+    --model_path "squad_model.pt" \
+    --train_path "datasets/squad_train.jsonl.gz" \
+    --dev_path   "datasets/squad_dev.jsonl.gz" \
+    --output_path "squad_predictions.txt" \
+    --rnn_cell_type "lstm" \
+    --hidden_dim 256 \
+    --bidirectional \
+    --dropout 0.3 \
+    --learning_rate 5e-4 \
+    --early_stop 3 \
+    --shuffle_examples \
+    --do_train \
+    --do_test
